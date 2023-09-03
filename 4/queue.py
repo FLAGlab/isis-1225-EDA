a4c2585e004bbc2f54d0ddc868d9ce74ec78892a
ncardozo <n.cardozo@uniandes.edu.co>
2023-09-03 17:05:51 -0500

    template for the stack and the queue
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ 4/queue.py
@@ -0,0 +1,41 @@
+"""
+ Queues are implemented as lists, where enqueue adds to 
+ the end of the list and dequeue removes the first element
+"""
+
+def new_queue():
+    """
+    Create an empty queue
+    """
+    pass
+
+def enqueue():
+    """
+    Add an element to the end of the queue
+    """
+    pass
+
+def dequeue():
+    """
+    Remove the first element from the front of the queue
+    all elements move to the front one position
+    """
+    pass
+
+def peek():
+    """
+    Check the first element of the queue
+    """
+    pass
+
+def is_empty():
+    """
+    Verifies if the queue is empty
+    """
+    pass
+
+def size():
+    """
+    returns the number of elements in the queue
+    """
+    pass
---
+++ 4/stack.py
@@ -0,0 +1,36 @@
+"""
+ Queues are implemented as lists, where push adds to 
+ the start of the list and pop removes the first element
+"""
+def new_stack():
+    """
+    Create an empty stack
+    """
+    pass
+
+def push():
+    """
+    Adds an element to the top of the stack
+    """
+    pass
+
+def pop():
+    """
+    Removes the top element of the stack
+    """
+    pass
+
+def top():
+    """
+    Returns the top element of the stack without removing it
+    """
+    pass
+
+def is_empty():
+    """
+    Verifies if the stack is empty
+    """
+    pass
+
+def size():
+    pass
