deba3609a3d6e3692cd7173ec85ea3dd91931d48
ncardozo <n.cardozo@uniandes.edu.co>
2024-02-19 09:05:02 -0500

    adding quick template
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

--- w7/quick.py
+++ w7/quick.py
@@ -1,2 +1,13 @@
-import recursivesinglelinkedlistv3 as List
 
+def quick_sort(l):
+    """
+    Orders the list l with respect to a pivot, putting all lesser elements, then all equal
+    and then all greater
+    """
+    pass
+
+
+#API
+l = [1, 25, 16, 12, 22, 0, 64, 42, 6]
+l2 = quick_sort(l)
+print(l2) 
