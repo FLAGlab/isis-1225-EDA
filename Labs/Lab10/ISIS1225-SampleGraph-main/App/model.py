37b31b8ab66b2f54260ba0f5d03fcc96d5ec58e5
ncardozo <n.cardozo@uniandes.edu.co>
2024-04-17 18:47:36 +0100

    lab10 code
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ Labs/Lab10/ISIS1225-SampleGraph-main/App/model.py
@@ -0,0 +1,276 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Contribución de:
+ *
+ * Dario Correal
+ *
+ """
+import config
+assert config
+
+"""
+En este archivo definimos los TADs que vamos a usar y las operaciones
+de creacion y consulta sobre las estructuras de datos.
+"""
+
+# -----------------------------------------------------
+#                       API
+# -----------------------------------------------------
+
+
+def newAnalyzer():
+    """ Inicializa el analizador
+
+   stops: Tabla de hash para guardar los vertices del grafo
+   connections: Grafo para representar las rutas entre estaciones
+   components: Almacena la informacion de los componentes conectados
+   paths: Estructura que almancena los caminos de costo minimo desde un
+           vertice determinado a todos los otros vértices del grafo
+    """
+    try:
+        analyzer = {
+            'stops': None,
+            'connections': None,
+            'components': None,
+            'paths': None
+        }
+
+        analyzer['stops'] = #TODO new_ht_linear_probing
+
+        analyzer['connections'] = #TODO new_directed_graph
+        return analyzer
+    except Exception as exp:
+        raise(Exception, 'model:newAnalyzer')
+
+
+# Funciones para agregar informacion al grafo
+
+def addStopConnection(analyzer, lastservice, service):
+    """
+    Adiciona las estaciones al grafo como vertices y arcos entre las
+    estaciones adyacentes.
+
+    Los vertices tienen por nombre el identificador de la estacion
+    seguido de la ruta que sirve.  Por ejemplo:
+
+    75009-10
+
+    Si la estacion sirve otra ruta, se tiene: 75009-101
+    """
+    try:
+        origin = formatVertex(lastservice)
+        destination = formatVertex(service)
+        cleanServiceDistance(lastservice, service)
+        distance = float(service['Distance']) - float(lastservice['Distance'])
+        distance = abs(distance)
+        addStop(analyzer, origin)
+        addStop(analyzer, destination)
+        addConnection(analyzer, origin, destination, distance)
+        addRouteStop(analyzer, service)
+        addRouteStop(analyzer, lastservice)
+        return analyzer
+    except Exception as exp:
+        raise(exp, 'model:addStopConnection')
+
+
+def addStop(analyzer, stopid):
+    """
+    Adiciona una estación como un vertice del grafo
+    """
+    try:
+        if not #TODO contains_vertex(analyzer['connections'], stopid):
+            #TODO insert_vertex(analyzer['connections'], stopid)
+        return analyzer
+    except Exception as exp:
+        raise(exp, 'model:addstop')
+
+
+def addRouteStop(analyzer, service):
+    """
+    Agrega a una estacion, una ruta que es servida en ese paradero
+    """
+    entry = m.get(analyzer['stops'], service['BusStopCode'])
+    if entry is None:
+        lstroutes = #TODO new_list(cmpfunction=compareroutes)
+        #TODO add_last(lstroutes, service['ServiceNo'])
+        #TODO put(analyzer['stops'], service['BusStopCode'], lstroutes)
+    else:
+        lstroutes = entry['value']
+        info = service['ServiceNo']
+        if not #TODO is_present(lstroutes, info):
+            #TODO add_last(lstroutes, info)
+    return analyzer
+
+
+def addRouteConnections(analyzer):
+    """
+    Por cada vertice (cada estacion) se recorre la lista
+    de rutas servidas en dicha estación y se crean
+    arcos entre ellas para representar el cambio de ruta
+    que se puede realizar en una estación.
+    """
+    lststops = #TODO keys(analyzer['stops'])
+    for key in #TODO iterator(lststops):
+        lstroutes = #TODO get(analyzer['stops'], key)['value']
+        prevrout = None
+        for route in #TODO iterator(lstroutes):
+            route = key + '-' + route
+            if prevrout is not None:
+                addConnection(analyzer, prevrout, route, 0)
+                addConnection(analyzer, route, prevrout, 0)
+            prevrout = route
+
+
+def addConnection(analyzer, origin, destination, distance):
+    """
+    Adiciona un arco entre dos estaciones
+    """
+    edge = #TODO get_edge(analyzer['connections'], origin, destination)
+    if edge is None:
+        #TODO addedge(analyzer['connections'], origin, destination, distance)
+    return analyzer
+
+# ==============================
+# Funciones de consulta
+# ==============================
+
+
+def connectedComponents(analyzer):
+    """
+    Calcula los componentes conectados del grafo
+    Se utiliza el algoritmo de Kosaraju
+    """
+    analyzer['components'] = #TODO KosarajuSCC(analyzer['connections'])
+    return #TODO connected_components(analyzer['components'])
+
+
+def minimumCostPaths(analyzer, initialStation):
+    """
+    Calcula los caminos de costo mínimo desde la estacion initialStation
+    a todos los demas vertices del grafo
+    """
+    analyzer['paths'] = #TODO dijkstra(analyzer['connections'], initialStation)
+    return analyzer
+
+
+def hasPath(analyzer, destStation):
+    """
+    Indica si existe un camino desde la estacion inicial a la estación destino
+    Se debe ejecutar primero la funcion minimumCostPaths
+    """
+    return #TODO  has_path_to(analyzer['paths'], destStation)
+
+
+def minimumCostPath(analyzer, destStation):
+    """
+    Retorna el camino de costo minimo entre la estacion de inicio
+    y la estacion destino
+    Se debe ejecutar primero la funcion minimumCostPaths
+    """
+    path = #TODO path_to(analyzer['paths'], destStation)
+    return path
+
+
+def totalStops(analyzer):
+    """
+    Retorna el total de estaciones (vertices) del grafo
+    """
+    return #TODO numVertices(analyzer['connections'])
+
+
+def totalConnections(analyzer):
+    """
+    Retorna el total arcos del grafo
+    """
+    return #TODO numEdges(analyzer['connections'])
+
+
+def servedRoutes(analyzer):
+    """
+    Retorna la estación que sirve a mas rutas.
+    Si existen varias rutas con el mismo numero se
+    retorna una de ellas
+    """
+    lstvert = #TODO keys(analyzer['stops'])
+    maxvert = None
+    maxdeg = 0
+    for vert in #TODO iterator(lstvert):
+        lstroutes = #TODO get(analyzer['stops'], vert)['value']
+        degree = #TODO size(lstroutes)
+        if(degree > maxdeg):
+            maxvert = vert
+            maxdeg = degree
+    return maxvert, maxdeg
+
+
+# ==============================
+# Funciones Helper
+# ==============================
+
+def cleanServiceDistance(lastservice, service):
+    """
+    En caso de que el archivo tenga un espacio en la
+    distancia, se reemplaza con cero.
+    """
+    if service['Distance'] == '':
+        service['Distance'] = 0
+    if lastservice['Distance'] == '':
+        lastservice['Distance'] = 0
+
+
+def formatVertex(service):
+    """
+    Se formatea el nombrer del vertice con el id de la estación
+    seguido de la ruta.
+    """
+    name = service['BusStopCode'] + '-'
+    name = name + service['ServiceNo']
+    return name
+
+
+# ==============================
+# Funciones de Comparacion
+# ==============================
+
+
+def compareStopIds(stop, keyvaluestop):
+    """
+    Compara dos estaciones
+    """
+    stopcode = keyvaluestop['key']
+    if (stop == stopcode):
+        return 0
+    elif (stop > stopcode):
+        return 1
+    else:
+        return -1
+
+
+def compareroutes(route1, route2):
+    """
+    Compara dos rutas
+    """
+    if (route1 == route2):
+        return 0
+    elif (route1 > route2):
+        return 1
+    else:
+        return -1
