e29c8cc2fe13c8fe8106e0b2104bae3eb6d4dad5
ncardozo <n.cardozo@uniandes.edu.co>
2024-05-09 09:32:33 -0500

    lab11
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ Labs/Lab11/ISIS1225-SamplePaths/App/model.py
@@ -0,0 +1,341 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this progra  If not, see <http://www.gnu.org/licenses/>.
+ * Contribución de:
+ *
+ * Dario Correal
+ *
+ """
+import config
+# TODO Lab 11, agregar importaciones dfs y bfs
+assert config
+
+"""
+En este archivo definimos los TADs que vamos a usar y las operaciones
+de creacion y consulta sobre las estructuras de datos.
+"""
+
+# -----------------------------------------------------
+#                       API
+# -----------------------------------------------------
+
+
+def newAnalyzer():
+    """ Inicializa el analizador
+
+   stops: Tabla de hash para guardar los vertices del grafo
+   connections: Grafo para representar las rutas entre estaciones
+   components: Almacena la informacion de los componentes conectados
+   paths: Estructura que almancena los caminos de costo minimo desde un
+           vertice determinado a todos los otros vértices del grafo
+    """
+    try:
+        # TODO Lab 11, agregar llave "search" para usar dfs y bfs
+        analyzer = {
+            'stops': None,
+            'connections': None,
+            'components': None,
+            'paths': None,
+        }
+
+        analyzer['stops'] = newMap()
+
+        analyzer['connections'] = newGraph()
+        return analyzer
+    except Exception as exp:
+        error.reraise(exp, 'model:newAnalyzer')
+
+
+# Funciones para agregar informacion al grafo
+
+def addStopConnection(analyzer, lastservice, service):
+    """
+    Adiciona las estaciones al grafo como vertices y arcos entre las
+    estaciones adyacentes.
+
+    Los vertices tienen por nombre el identificador de la estacion
+    seguido de la ruta que sirve.  Por ejemplo:
+
+    75009-10
+
+    Si la estacion sirve otra ruta, se tiene: 75009-101
+    """
+    try:
+        origin = formatVertex(lastservice)
+        destination = formatVertex(service)
+        cleanServiceDistance(lastservice, service)
+        distance = float(service['Distance']) - float(lastservice['Distance'])
+        distance = abs(distance)
+        addStop(analyzer, origin)
+        addStop(analyzer, destination)
+        addConnection(analyzer, origin, destination, distance)
+        addRouteStop(analyzer, service)
+        addRouteStop(analyzer, lastservice)
+        return analyzer
+    except Exception as exp:
+        error.reraise(exp, 'model:addStopConnection')
+
+
+def addStop(analyzer, stopid):
+    """
+    Adiciona una estación como un vertice del grafo
+    """
+    try:
+        if not containsVertex(analyzer['connections'], stopid):
+            insertVertex(analyzer['connections'], stopid)
+        return analyzer
+    except Exception as exp:
+        error.reraise(exp, 'model:addstop')
+
+
+def addRouteStop(analyzer, service):
+    """
+    Agrega a una estacion, una ruta que es servida en ese paradero
+    """
+    entry = get(analyzer['stops'], service['BusStopCode'])
+    if entry is None:
+        lstroutes = newList(cmpfunction=compareroutes)
+        addLast(lstroutes, service['ServiceNo'])
+        put(analyzer['stops'], service['BusStopCode'], lstroutes)
+    else:
+        lstroutes = entry['value']
+        info = service['ServiceNo']
+        if not isPresent(lstroutes, info):
+            addLast(lstroutes, info)
+    return analyzer
+
+
+def addRouteConnections(analyzer):
+    """
+    Por cada vertice (cada estacion) se recorre la lista
+    de rutas servidas en dicha estación y se crean
+    arcos entre ellas para representar el cambio de ruta
+    que se puede realizar en una estación.
+    """
+    lststops = keySet(analyzer['stops'])
+    for key in iterator(lststops):
+        lstroutes = get(analyzer['stops'], key)['value']
+        prevrout = None
+        for route in iterator(lstroutes):
+            route = key + '-' + route
+            if prevrout is not None:
+                addConnection(analyzer, prevrout, route, 0)
+                addConnection(analyzer, route, prevrout, 0)
+            prevrout = route
+
+
+def addConnection(analyzer, origin, destination, distance):
+    """
+    Adiciona un arco entre dos estaciones
+    """
+    edge = getEdge(analyzer['connections'], origin, destination)
+    if edge is None:
+        addEdge(analyzer['connections'], origin, destination, distance)
+    return analyzer
+
+# ==============================
+# Funciones de consulta
+# ==============================
+
+
+def connectedComponents(analyzer):
+    """
+    Calcula los componentes conectados del grafo
+    Se utiliza el algoritmo de Kosaraju
+    """
+    analyzer['components'] = KosarajuSCC(analyzer['connections'])
+    return connectedComponents(analyzer['components'])
+
+
+def minimumCostPaths(analyzer, initialStation):
+    """
+    Calcula los caminos de costo mínimo desde la estacion initialStation
+    a todos los demas vertices del grafo
+    """
+    analyzer['paths'] = Dijkstra(analyzer['connections'], initialStation)
+    return analyzer
+
+
+def hasPath(analyzer, destStation):
+    """
+    Indica si existe un camino desde la estacion inicial a la estación destino
+    Se debe ejecutar primero la funcion minimumCostPaths
+    """
+    return hasPathTo(analyzer['paths'], destStation)
+
+
+def minimumCostPath(analyzer, destStation):
+    """
+    Retorna el camino de costo minimo entre la estacion de inicio
+    y la estacion destino
+    Se debe ejecutar primero la funcion minimumCostPaths
+    """
+    path = pathTo(analyzer['paths'], destStation)
+    return path
+
+
+def searchPaths(analyzer, initialStation, method):
+    """
+    searchPaths Calcula los caminos posibles desde una estacion de origen
+    y puede utilizar los algoritmos "dfs" o "bfs"
+
+    Args:
+        analyzer (dict): diccionario con las estructuras de datos del modelo
+        originStation (vertice): estacion de origen del recorrido
+        method (str, optional): algoritmo de busqueda. Por defecto es "dfs"
+
+    Returns:
+        dict: devuelve el analyzer del modelo
+    """
+    # TODO Lab 11, ejecutar DepthFirstSearch de dfs
+    if method == "dfs":
+        pass
+    # TODO Lab 11, ejecutar BreadhtFisrtSearch de bfs
+    elif method == "bfs":
+        pass
+    return analyzer
+
+
+def hasSearchPath(analyzer, destStation, method):
+    """
+    hasSearchPath indica si existe un camino desde la estacion inicial a
+    la estación destino. Se debe ejecutar primero la funcion searchPaths()
+
+    Args:
+        analyzer (dict): diccionario con las estructuras de datos del modelo
+        destStation (vertice): estacion de destino para el recorrido
+        method (str, optional): algoritmo de busqueda. Por defecto es "dfs"
+    """
+    # TODO Lab 11, ejecutar hasPathTo por dfs
+    if method == "dfs":
+        return None
+    # TODO Lab 11, ejecutar hasPathTo por bfs
+    elif method == "bfs":
+        return None
+
+
+def searchPathTo(analyzer, destStation, method):
+    """
+    searchPath retorna el camino de encontrado entre la estacion de inicio
+    y la estacion destino Se debe ejecutar primero la funcion searchPaths
+
+    Args:
+        analyzer (dict): diccionario con las estructuras de datos del modelo
+        destStation (vertice): estacion de destino para el recorrido
+        method (str, optional): algoritmo de busqueda. Por defecto es "dfs"
+
+    Returns:
+        stack: devuele una pila con el camino encontrado en la busqueda.
+    """
+    path = None
+    # TODO Lab 11, ejecutar pathTo por dfs
+    if method == "dfs":
+        pass
+    # TODO Lab 11, ejecutar pathTo por bfs
+    elif method == "bfs":
+        pass
+    return path
+
+
+def totalStops(analyzer):
+    """
+    Retorna el total de estaciones (vertices) del grafo
+    """
+    return numVertices(analyzer['connections'])
+
+
+def totalConnections(analyzer):
+    """
+    Retorna el total arcos del grafo
+    """
+    return numEdges(analyzer['connections'])
+
+
+def servedRoutes(analyzer):
+    """
+    Retorna la estación que sirve a mas rutas.
+    Si existen varias rutas con el mismo numero se
+    retorna una de ellas
+    """
+    lstvert = keySet(analyzer['stops'])
+    maxvert = None
+    maxdeg = 0
+    for vert in iterator(lstvert):
+        lstroutes = get(analyzer['stops'], vert)['value']
+        degree = size(lstroutes)
+        if(degree > maxdeg):
+            maxvert = vert
+            maxdeg = degree
+    return maxvert, maxdeg
+
+
+# ==============================
+# Funciones Helper
+# ==============================
+
+def cleanServiceDistance(lastservice, service):
+    """
+    En caso de que el archivo tenga un espacio en la
+    distancia, se reemplaza con cero.
+    """
+    if service['Distance'] == '':
+        service['Distance'] = 0
+    if lastservice['Distance'] == '':
+        lastservice['Distance'] = 0
+
+
+def formatVertex(service):
+    """
+    Se formatea el nombrer del vertice con el id de la estación
+    seguido de la ruta.
+    """
+    name = service['BusStopCode'] + '-'
+    name = name + service['ServiceNo']
+    return name
+
+
+# ==============================
+# Funciones de Comparacion
+# ==============================
+
+
+def compareStopIds(stop, keyvaluestop):
+    """
+    Compara dos estaciones
+    """
+    stopcode = keyvaluestop['key']
+    if (stop == stopcode):
+        return 0
+    elif (stop > stopcode):
+        return 1
+    else:
+        return -1
+
+
+def compareroutes(route1, route2):
+    """
+    Compara dos rutas
+    """
+    if (route1 == route2):
+        return 0
+    elif (route1 > route2):
+        return 1
+    else:
+        return -1
---
+++ Labs/Lab11/ISIS1225-SamplePaths/App/view.py
@@ -0,0 +1,197 @@
+"""
+ * Copyright 2020, Departamento de sistemas y Computación
+ * Universidad de Los Andes
+ *
+ *
+ * Desarrolado para el curso ISIS1225 - Estructuras de Datos y Algoritmos
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Contribución de:
+ *
+ * Dario Correal
+ *
+ """
+
+
+import sys
+import config
+import threading
+from App import controller
+assert config
+
+"""
+La vista se encarga de la interacción con el usuario.
+Presenta el menu de opciones  y  por cada seleccion
+hace la solicitud al controlador para ejecutar la
+operación seleccionada.
+"""
+
+# ___________________________________________________
+#  Variables
+# ___________________________________________________
+
+
+servicefile = 'bus_routes_14000.csv'
+initialStation = None
+searchMethod = None
+
+# ___________________________________________________
+#  Menu principal
+# ___________________________________________________
+
+
+def printMenu():
+    print("\n")
+    print("*******************************************")
+    # TODO Lab 11, asegurarse de completar las opciones 5, 9 y 10
+    print("Bienvenido")
+    print("1- Inicializar Analizador")
+    print("2- Cargar información de buses de singapur")
+    print("3- Calcular componentes conectados")
+    print("4- Establecer estación base:")
+    print("5- Establecer metodo de busqueda y estación base:")
+    print("6- Hay camino entre estacion base y estación: ")
+    print("7- Ruta de costo mínimo desde la estación base y estación: ")
+    print("8- Estación que sirve a mas rutas: ")
+    print("9- Existe un camino de busqueda entre base y estación: ")
+    print("10- Ruta de busqueda entre la estación base y estación: ")
+    print("0- Salir")
+    print("*******************************************")
+
+
+def optionTwo(cont):
+    print("\nCargando información de transporte de singapur ....")
+    controller.loadServices(cont, servicefile)
+    numedges = controller.totalConnections(cont)
+    numvertex = controller.totalStops(cont)
+    print('Numero de vertices: ' + str(numvertex))
+    print('Numero de arcos: ' + str(numedges))
+    print('El limite de recursion actual: ' + str(sys.getrecursionlimit()))
+
+
+def optionThree(cont):
+    print('El número de componentes conectados es: ' +
+          str(controller.connectedComponents(cont)))
+
+
+def optionFour(cont, initialStation):
+    print('Calculando costo de caminos')
+    controller.minimumCostPaths(cont, initialStation)
+    print("FIN!")
+
+
+def optionFive(cont, initialStation, searchMethod):
+    # TODO Lab 11, conectar con la funcion del controller searchPaths
+    pass
+
+
+def optionSix(cont, initialStation, destStation):
+    haspath = controller.hasPath(cont, destStation)
+    print('Hay camino entre la estación base '+ initialStation +
+          ' y la estación: ' + destStation + '?: ')
+    print(haspath)
+
+
+def optionSeven(cont, destStation):
+    path = controller.minimumCostPath(cont, destStation)
+    if path is not None:
+        pathlen = stack.size(path)
+        print('El camino es de longitud: ' + str(pathlen))
+        while (not stack.isEmpty(path)):
+            stop = stack.pop(path)
+            print(stop)
+    else:
+        print('No hay camino')
+
+
+def optionEight(cont):
+    maxvert, maxdeg = controller.servedRoutes(cont)
+    print('Estación: ' + maxvert + '  Total rutas servidas: '
+          + str(maxdeg))
+
+
+def optionNine(cont, destStation, searchMethod):
+    # TODO Lab 11, conectar con la funcion del controller hasSearchPath
+    haspath = None
+    print(haspath)
+
+
+def optionTen(cont, destStation, searchMethod):
+    # TODO Lab 11, conectar con la funcion del controller searchPathTo
+    path = None
+    if path is not None:
+        pass
+    else:
+        print('No hay camino')
+
+
+"""
+Menu principal
+"""
+
+
+def thread_cycle():
+    initialStation  = None
+    while True:
+        printMenu()
+        inputs = input('Seleccione una opción para continuar\n>')
+        if int(inputs) == 1:
+            print("\nInicializando....")
+            # cont es el controlador que se usará de acá en adelante
+            cont = controller.init()
+
+        elif int(inputs) == 2:
+            optionTwo(cont)
+
+        elif int(inputs) == 3:
+            optionThree(cont)
+
+        elif int(inputs) == 4:
+            msg = "Estación Base: BusStopCode-ServiceNo (Ej: 75009-10): "
+            initialStation = input(msg)
+            optionFour(cont, initialStation)
+
+        elif int(inputs) == 5:
+            # TODO Lab 11, completar inputs opt 5, searchMethod, initialStation
+            pass
+
+        elif int(inputs) == 6:
+            destStation = input("Estación destino (Ej: 15151-10): ")
+            optionSix(cont, initialStation, destStation)
+
+        elif int(inputs) == 7:
+            destStation = input("Estación destino (Ej: 15151-10): ")
+            optionSeven(cont, destStation)
+
+        elif int(inputs) == 8:
+            optionEight(cont)
+
+        elif int(inputs) == 9:
+            # TODO Lab 11, completar inputs opt 9, destStation
+            pass
+
+        elif int(inputs) == 10:
+            # TODO Lab 11, completar inputs opt 10, destStation
+            pass
+
+        else:
+            sys.exit(0)
+
+
+if __name__ == "__main__":
+    threading.stack_size(67108864)  # 64MB stack
+    sys.setrecursionlimit(2 ** 20)
+    thread = threading.Thread(target=thread_cycle)
+    thread.start()
