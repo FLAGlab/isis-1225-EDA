415fde52aed3ab345c186f30d209ea8917be09d9
ncardozo <n.cardozo@uniandes.edu.co>
2024-05-09 09:59:18 -0500

    adding rbt code
    Signed-off-by: ncardozo <n.cardozo@uniandes.edu.co>

---
+++ w10/rbt.py
@@ -0,0 +1,54 @@
+
+RED = 0
+BLACK = 1
+
+def create_node(value):
+    pass
+
+def create_rbt(value):
+    pass
+
+def is_empty(rbt):
+    pass
+
+def search(rbt, value):
+    pass
+    
+def insert(rbt, value):
+    pass
+    
+
+def is_red(rbt):
+    pass
+
+def color_flip(rbt):
+    pass
+
+def rotate_left(rbt):
+    pass
+
+
+def rotate_right(rbt):
+    pass
+
+rbt = create_rbt(10, 10)
+insert(rbt, 8, 8)
+insert(rbt, 20, 20)
+print(insert(rbt, 7, 7))
+ 
+
+# rbt = create_rbt(10, 10)
+# insert(rbt, 8, 8)
+# insert(rbt, 39, 39)
+# insert(rbt, 58, 58)
+# insert(rbt, 45, 45)
+# insert(rbt, 23, 23)
+# insert(rbt, 14, 14)
+# insert(rbt, 90, 90)
+# insert(rbt, 83, 83)
+# insert(rbt, 34, 34)
+# insert(rbt, 2, 2)
+# insert(rbt, 9, 9)
+# insert(rbt, 7, 7)
+# insert(rbt, 1, 1)
+
